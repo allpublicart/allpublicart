9b8e6a0256164588673bc9b22baeca7fdf73a734
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2017-09-02 13:56:04 +0200

    [Feature] Add maxSupply and placeholder companyAllocation.

--- contracts/AllPublicArtToken.sol
+++ contracts/AllPublicArtToken.sol
@@ -10,4 +10,16 @@ contract AllPublicArtToken is MintableToken {
     string public constant name = "All Public Art";
     string public constant symbol = "APA";
     uint8 public constant decimals = 18;
+    uint256 public maxSupply = 1000000000e18;
+    uint256 public companyAllocation = 10000000e18; // Ask Graham whether there is company allocation. Used 10 % of total token supply for now.
+
+    function AllPublicArtToken(address company) {
+        // allocate tokens for company
+        mint(company, companyAllocation);
+    }
+
+    function mint(address _to, uint256 _amount) onlyOwner canMint returns (bool) {
+        assert(totalSupply <= maxSupply);
+        super.mint(_to, _amount);
+    }
 }
