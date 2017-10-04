e34f35177e8ea410af460b0b6947051403bcc36b
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2017-10-04 14:40:35 +0200

    [Feature] Add burnable token.

--- contracts/AllPublicArtToken.sol
+++ contracts/AllPublicArtToken.sol
@@ -2,13 +2,22 @@ pragma solidity ^0.4.13;
 
 import "zeppelin-solidity/contracts/token/MintableToken.sol";
 import "zeppelin-solidity/contracts/token/PausableToken.sol";
+import "zeppelin-solidity/contracts/token/BurnableToken.sol";
 
 /**
  * @title All Public Art Token contract - ERC20 compatible token contract.
  * @author Gustavo Guimaraes - <gustavoguimaraes@gmail.com>
  */
-contract AllPublicArtToken is PausableToken, MintableToken {
+contract AllPublicArtToken is BurnableToken, PausableToken, MintableToken {
     string public constant name = "All Public Art";
     string public constant symbol = "APA";
     uint8 public constant decimals = 18;
+
+    /**
+     * @dev makes a number token unused forever
+     * @param _value Number of tokens to burn
+     */
+    function burn(uint256 _value) public {
+        super.burn(_value);
+    }
 }
